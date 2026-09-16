11bef12f82aecdaac7422171fa237c3cadba98b6
Ahmed Amine Berdouz <berdouzamine1@gmail.com>
2026-09-16 22:26:23 +0100

    Fix: restore missing facadeMarkerItems variable

--- src/app/components/FacadeViewer.tsx
+++ src/app/components/FacadeViewer.tsx
@@ -65,6 +65,7 @@ export default function FacadeViewer() {
   };
   const magasinPositions = FACADE_MAGASIN_POSITIONS[currentImageIndex] ?? {};
   const studios = apartmentTypes.filter((a) => a.category === 'studio');
+  const facadeMarkerItems = [...facadeApartments, ...studios];
   // Masquer temporairement les magasins des facades (prets a etre reactivees plus tard)
   const SHOW_MAGASINS = false;
   const facadeMagasinItems = SHOW_MAGASINS ? facadeMagasins.filter((m) => magasinPositions[m.id]) : [];
